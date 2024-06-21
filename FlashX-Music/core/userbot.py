981e4a522ffc78ccdd8fd9cf9543d3448e0bb35e
Zenaku2050s <yusume65usm@gmail.com>
2024-06-21 19:02:31 +0630

    Update userbot.py

--- FlashX-Music/core/userbot.py
+++ FlashX-Music/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ALLTYPECC")
-                await self.one.join_chat("gitwizardbypass")
+                await self.one.join_chat("seriousvs_version10")
+                await self.one.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ALLTYPECC")
-                await self.one.join_chat("gitwizardbypass")
+                await self.two.join_chat("seriousvs_version10")
+                await self.one.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ALLTYPECC")
-                await self.one.join_chat("gitwizardbypass")
+                await self.three.join_chat("seriousvs_version10")
+                await self.one.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ALLTYPECC")
-                await self.one.join_chat("gitwizardbypass")
+                await self.four.join_chat("seriousvs_version10")
+                await self.one.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("ALLTYPECC")
-                await self.one.join_chat("gitwizardbypass")
+                await self.five.join_chat("seriousvs_version10")
+                await self.one.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(5)
