57e6a57ac1a111f2eda15c637e5fa16f0074be06
Zenaku2050s <yusume65usm@gmail.com>
2024-06-21 17:32:21 +0630

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/FlashX-Music",
+    "https://github.com/Zenaku2050s/FlashX-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
@@ -143,21 +143,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
 STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
 SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/e69ee26e970b904123dba.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
